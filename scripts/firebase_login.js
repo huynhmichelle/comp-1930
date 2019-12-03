c14c7c2b9a0a768ca4dc6fc3d2d14d4e1750697d
tj@box <chalkcode@protonmail.com>
2019-12-02 19:44:58 -0800

    consolidated live site and recent changes (hopefully)

--- scripts/firebase_login.js
+++ scripts/firebase_login.js
@@ -1,6 +1,6 @@
 function firebaseLogin() {
     console.log('firebase login called');
-    // Your web app's Firebase configuration
+    
     var firebaseConfig = {
         apiKey: "AIzaSyCOl7zfOtE5H75WPViBv-jNxn9nZLhHNFo",
         authDomain: "fire1-2c085.firebaseapp.com",
@@ -11,9 +11,14 @@ function firebaseLogin() {
         appId: "1:228703666474:web:3bacd2b08daf85a79cde53",
         measurementId: "G-8V89TB50QY"
     };
+
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     // Initialize the FirebaseUI Widget using Firebase.
+    // Initialize Firestore
+    var db = firebase.firestore();
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
     var uiConfig = {
         callbacks: {
@@ -21,7 +26,27 @@ function firebaseLogin() {
                 // User successfully signed in.
                 // Return type determines whether we continue the redirect automatically
                 // or whether we leave that to developer to handle.
-                return true;
+                console.log('LOGGED IN');
+                var user = authResult.user;
+                if (authResult.additionalUserInfo.isNewUser) {
+                    db.collection('users').doc(user.uid).set({
+                        email: user.email,
+                        // not checking if uniqe for noww
+                        displayID: randInt(1, 10e6)
+                    })
+                        .then(function () {
+                            console.log("new user added to firestore");
+                            window.location.assign('edit_user_profile.html');
+                            return true;
+                        })
+                        .catch(function (error) {
+                            console.log('Error happened' + error);
+                        });
+
+                } else {
+                    console.log('existing user');
+                    return true;
+                }
             },
             uiShown: function () {
                 // The widget is rendered.
@@ -42,4 +67,9 @@ function firebaseLogin() {
     };
     // The start method will wait until the DOM is loaded.
     ui.start('#firebaseui-auth-container', uiConfig);
+
+    function randInt(min, max){
+        return(Math.floor(Math.random() * (+max - +min)) + +min); 
+    }
+
 }
